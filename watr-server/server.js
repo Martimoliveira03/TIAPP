e994bb677a118db4e08b30458ffbbf199a6f74c8
mariabreuv <17maria.abreu@gmail.com>
2025-06-01 18:37:28 +0100

    login is working again

--- watr-server/server.js
+++ watr-server/server.js
@@ -11,9 +11,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static('public'));
+app.use('/watr-server', express.static(__dirname)); // add this
+
 
 const port = new SerialPort({
   path: 'COM7',
